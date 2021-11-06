012dd0bbc9f0dc3f74db7c128525dc1075db4c82
MedoOnlyOne <mohamed.medhat2199@gmail.com>
2021-11-06 21:54:35 +0200

    Signup and login forms

--- distributed_ecommerce/blueprints/auth.py
+++ distributed_ecommerce/blueprints/auth.py
@@ -17,8 +17,8 @@ def login():
             if bcrypt.check_password_hash(user.password, form.password.data):
                 login_user(user)
                 return redirect(url_for('home'))
-            return render_template('login.html', form=form, error_message="Incorrect password")
-        return render_template('login.html', form=form, error_message="Incorrect username")
+            return render_template('login.html', form=form, message="Incorrect password")
+        return render_template('login.html', form=form, message="Incorrect username")
 
     return render_template('login.html', form=form)
 
@@ -32,11 +32,11 @@ def logout():
 @auth.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = forms.SignUpForm()
-
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data)
         created_user = User(first_name=form.first_name.data, last_name=form.last_name.data, username=form.username.data, email=form.email.data, password=hashed_password, )
         db.session.add(created_user)
         db.session.commit()
         return redirect(url_for('auth.login'))
+    
     return render_template('signup.html', form=form)
